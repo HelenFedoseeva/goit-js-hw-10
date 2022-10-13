2853854484a4a4aa7c22049df49d63cfd059bac6
Helen <helenlena52@gmail.com>
2022-10-13 10:55:12 +0300

    add-notiflix-lib

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,30 +1,37 @@
 import debounce from "lodash.debounce";
+import Notiflix from "notiflix";
 
 export const fetchCountriesName = (name) => {
-  fetch(`https://restcountries.com/v3.1/name/${name}`)
-    .then((response) => {
-      return response.json();
-    })
-    .then((country) => {
-      console.log(country);
-    })
-    .catch((error) => {
-      console.log(error);
-    })
-    .finally();
+  const url = `https://restcountries.com/v3.1/name/${name}`;
+
+  return fetch(url).then((response) => {
+    return response.json();
+  });
 };
 
 const countryRef = document.getElementById("search-box");
-let name = null;
 
 countryRef.addEventListener("input", debounce(onInputHandler, 300));
 
 function onInputHandler(event) {
   event.preventDefault();
-  name = event.target.value;
-  console.log(name);
 
-  fetchCountriesName(name);
+  let name = event.target.value;
+  let resultName = name.trim();
+
+  fetchCountriesName(resultName)
+    .then((country) => {
+      console.log(country);
+      if (country.length > 10) {
+        Notiflix.Notify.info(
+          "Too many matches found. Please enter a more specific name."
+        );
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+    .finally();
 }
 
 function renderCountryCard(country) {
